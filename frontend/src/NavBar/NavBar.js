5fd7fb218c40aced105b231d284ebf6830272707
Roger Delgado <rogmide@gmail.com>
2023-01-11 15:47:04 -0500

    adding resposive website

--- frontend/src/NavBar/NavBar.js
+++ frontend/src/NavBar/NavBar.js
@@ -3,43 +3,113 @@ import { Link, NavLink } from "react-router-dom";
 import UserContext from "../UseContext";
 import "./NavBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaw, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPaw,
+  faSearch,
+  faHeart,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+
+const SearchHeartIcons = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <NavLink className="nav-link   mouseOverIcon" to="/">
+        <FontAwesomeIcon style={{ color: "5c717a" }} icon={faSearch} />
+      </NavLink>
+      <NavLink className="nav-link  mouseOverIcon" to="/login">
+        <FontAwesomeIcon style={{ color: "5c717a" }} icon={faHeart} />
+      </NavLink>
+    </div>
+  );
+};
 
 const NoUserLogIn = () => {
   return (
     <ul className="navbar-nav ml-auto iconSearch">
-      <li className="nav-item mr-4">
-        <div>
-          <NavLink className="nav-link mouseOverIcon" to="/login">
-            <FontAwesomeIcon style={{ color: "5c717a" }} icon={faSearch} />
-          </NavLink>
-        </div>
-      </li>
+      <div className="bigScreen">{SearchHeartIcons()}</div>
       <li className="nav-item mr-4">
         <NavLink className="nav-link" to="/login">
           <span className="nav-btn">Login</span>
         </NavLink>
       </li>
-      {/* <li className="nav-item mr-4">
+      <li className="nav-item mr-4">
         <NavLink className="nav-link" to="/signup">
           <span className="nav-btn">Sign Up</span>
         </NavLink>
-      </li> */}
+      </li>
     </ul>
   );
 };
 
 const isLogIn = (name, logout) => {
   return (
+    <ul className="navbar-nav ml-auto iconSearch">
+      <li className="nav-item mr-4 bigScreen">{SearchHeartIcons()}</li>
+      <div className="dropdown">
+        <button
+          className="dropdown-toggle nav-btn"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          {name}
+        </button>
+        <div
+          className="dropdown-menu dropMenu "
+          aria-labelledby="dropdownMenuButton"
+        >
+          <div className="dropMenuItemHolder">
+            <NavLink
+              className="nav-link active dropMenuItem"
+              to="/profile"
+              // onClick={logout}
+            >
+              Account Info
+            </NavLink>
+            <NavLink
+              className="nav-link active dropMenuItem"
+              to="/"
+              onClick={logout}
+            >
+              My Recent Search
+            </NavLink>
+            <NavLink
+              className="nav-link active dropMenuItem"
+              to="/"
+              onClick={logout}
+            >
+              Sign out
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </ul>
+  );
+};
+
+const middleMenu = () => {
+  return (
     <ul className="navbar-nav ml-auto">
       <li className="nav-item mr-4">
         <NavLink className="nav-link" to="/profile">
-          Profile
+          Home
         </NavLink>
       </li>
       <li className="nav-item mr-4">
-        <NavLink className="nav-link" to="/" onClick={logout}>
-          Log out {name}
+        <NavLink className="nav-link" to="/">
+          About us
+        </NavLink>
+      </li>
+      <li className="nav-item mr-4">
+        <NavLink className="nav-link" to="/">
+          Organizations
+        </NavLink>
+      </li>
+      <li className="nav-item mr-4">
+        <NavLink className="nav-link" to="/">
+          Gallery
         </NavLink>
       </li>
     </ul>
@@ -50,10 +120,7 @@ const NavBar = () => {
   const { currUser, logout } = useContext(UserContext);
 
   return (
-    <nav
-      className="container navbar navbar-expand-lg iconSearch"
-      role="navigation"
-    >
+    <nav className="container navbar navbar-expand-lg " role="navigation">
       <Link className="navbar-brand" to="/">
         <div className="titleHolder">
           <FontAwesomeIcon className="titleIcon" icon={faPaw} />
@@ -63,29 +130,31 @@ const NavBar = () => {
           </span>
         </div>
       </Link>
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/profile">
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/" onClick={logout}>
-            About us
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/" onClick={logout}>
-            Organizations
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/" onClick={logout}>
-            Gallery
-          </NavLink>
-        </li>
-      </ul>
-      {currUser ? isLogIn(currUser.firstName, logout) : NoUserLogIn()}
+
+      <div className="smallScreen" style={{ display: "flex" }}>
+        {SearchHeartIcons()}
+      </div>
+      {/* <div className="smallScreen" style={{ display: "flex" }}>
+        <NavLink className="nav-link   mouseOverIcon" to="/">
+          <FontAwesomeIcon style={{ color: "5c717a" }} icon={faSearch} />
+        </NavLink>
+        <NavLink className="nav-link  mouseOverIcon" to="/login">
+          <FontAwesomeIcon style={{ color: "5c717a" }} icon={faHeart} />
+        </NavLink>
+      </div> */}
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <FontAwesomeIcon style={{ color: "5c717a" }} icon={faBars} />
+      </button>
+      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        {middleMenu()}
+        {currUser ? isLogIn(currUser.firstName, logout) : NoUserLogIn()}
+      </div>
     </nav>
   );
 };
