a8b647d82b1812f0ca9074a4a2add2be72c94ae1
Roger Delgado <rogmide@gmail.com>
2023-01-15 17:49:12 -0500

    adding validation somethig there is no pic on the pet

--- frontend/src/Home/HomePetCard.js
+++ frontend/src/Home/HomePetCard.js
@@ -51,18 +51,29 @@ const HomePetCard = () => {
         </div>
         <div className="backGroundCard"></div>
         <div className="cardHolder">
-          <img
-            className="imgHolder"
-            src={
-              pet
-                ? pet.primary_photo_cropped.full
-                : "https://images.unsplash.com/photo-1579187707643-35646d22b596?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
-            }
-            alt="Img Random Animal"
-          />
+          {pet && pet.primary_photo_cropped.full ? (
+            <img
+              className="imgHolder"
+              src={
+                pet
+                  ? pet.primary_photo_cropped.full
+                  : "https://images.unsplash.com/photo-1579187707643-35646d22b596?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+              }
+              alt="Img Random Animal"
+            />
+          ) : (
+            <img
+              className="imgHolder"
+              src={
+                "https://images.unsplash.com/photo-1579187707643-35646d22b596?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+              }
+              alt="Img Random Animal"
+            />
+          )}
+
           <div className="petData" style={{ display: "flex" }}>
             <p style={{ fontWeight: "bolder" }}>
-              {pet ? pet.name : "Loading..."}
+              {pet ? pet.name.slice(0, 19) : "Loading..."}
             </p>
             <p style={{ fontWeight: "bolder" }}>
               {pet ? pet.age : "Loading..."}
