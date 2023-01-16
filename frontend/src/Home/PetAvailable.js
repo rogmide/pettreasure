0bc13432e25a577cdb3fbaa52eb423915f75ce72
Roger Delgado <rogmide@gmail.com>
2023-01-16 13:10:43 -0500

    working on pet aviable

--- frontend/src/Home/PetAvailable.js
+++ frontend/src/Home/PetAvailable.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import PetTreasureApi from "../API/Api";
 import PetAvailableCard from "./PetAvailableCard";
 import "./PetAviable.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 const PetAviable = () => {
   const [pets, setPets] = useState();
@@ -37,6 +39,34 @@ const PetAviable = () => {
         </div>
         <div className="petHolder">
           {pets ? pets.map((p) => <PetAvailableCard key={p.id} pet={p} />) : ""}
+          <div className="cardMain1">
+            <div className="backGroundCard1"></div>
+            <div className="cardHolder1">
+              <div className="findMore">
+                <FontAwesomeIcon
+                  className="titleIcon iconHolder1"
+                  icon={faPaw}
+                />
+                <div
+                  className="petData1"
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                  <p style={{ fontSize: "16.9px", textAlign: "center", marginBottom: "30px" }}>
+                    6000+ pets available on{" "}
+                    <span style={{ color: "darkred" }}>Pet</span>Treasure{" "}
+                  </p>
+                </div>
+
+                <span className="titlePage">
+                  {" "}
+                  <p style={{ fontSize: "16.9px", textAlign: "center", marginTop: "10px" }}>
+                    {" "}
+                    MEET THEM{" "}
+                  </p>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
