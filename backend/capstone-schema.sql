11f7f333dcacc2ece468ada3664134b3afbf4e7e
Roger Delgado <rogmide@gmail.com>
2023-01-28 11:43:52 -0500

    changing DB schema for favorite pet and user zip code

--- backend/capstone-schema.sql
+++ backend/capstone-schema.sql
@@ -5,20 +5,25 @@
 --   description TEXT NOT NULL,
 --   logo_url TEXT
 -- );
-
--- CREATE TABLE users (
---   username varchar(25) PRIMARY KEY,
---   password TEXT NOT NULL,
---   first_name text NOT NULL,
---   last_name text NOT NULL,
---   email text NOT NULL CHECK (position('@' IN email) > 1),
---   is_admin boolean NOT NULL DEFAULT FALSE
--- );
+CREATE TABLE users (
+  username varchar(25) PRIMARY KEY,
+  password TEXT NOT NULL,
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  email text NOT NULL CHECK (position('@' IN email) > 1),
+  is_admin boolean NOT NULL DEFAULT FALSE,
+  zip_code integer
+);
 
 CREATE TABLE config (
   api_key text PRIMARY KEY,
   secret_key text NOT NULL,
   api_token text NOT NULL
+);
+
+CREATE TABLE favorite (
+  user_id varchar(25) PRIMARY KEY,
+  pet_id integer NOT NULL
 );
 
 -- CREATE TABLE applications (
